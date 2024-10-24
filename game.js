5c90ace97a5b5b600fe485e7d76cbd203cf8a8c1
SnowmanZak <frostbitezak@gmail.com>
2024-10-24 09:50:46 -0400

    changing the cells to have buttons inside of them and fitting the buttons to the cells

--- game.js
+++ game.js
@@ -8,6 +8,6 @@ function selectSymbol(symbol) {
     player1Symbol = symbol;
     player2Symbol = symbol === "X" ? "O" : "X"; 
     currentPlayer = player1Symbol;
-    isGameStarted = true; 
+    isGameStarted = true;
     alert(`Player 1 is ${player1Symbol} and Player 2 is ${player2Symbol}. Game starts!`);
 }
