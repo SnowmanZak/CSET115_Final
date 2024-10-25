91bfcc2a402862913f47510ffa30f6d597d1dbb9
Jayden <jaydenwinters5@gmail.com>
2024-10-25 07:54:20 -0400

    made win condition function

--- game.js
+++ game.js
@@ -11,3 +11,22 @@ function selectSymbol(symbol) {
     isGameStarted = true; 
     alert(`Player 1 is ${player1Symbol} and Player 2 is ${player2Symbol}. Game starts!`);
 }
+
+function checkWinner() {
+    const winningCombinations = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6],
+    ];
+
+    return winningCombinations.some(combination => {
+        const [a, b, c] = combination;
+        return board[a] && board[a] === board[b] && board[a] === board[c];
+    });
+}
+
